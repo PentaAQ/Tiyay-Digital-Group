5087a5832712cb0def18777a69f499f6253af934
“PentaAQ” <eddylxz@gmail.com>
2025-06-27 21:38:46 -0500

    Full Page Desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,5 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        '@': '../',
-      },
-    },
   },
 });
